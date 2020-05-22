31be93a36ff659737b4d1074f226ee2201c83f1d
sh02562 <shweta02562@gmail.com>
2020-05-22 17:55:28 +0530

    Changes Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
 import { fromEvent, Subscription, Observable } from 'rxjs';
 import { takeUntil, map, mergeMap, filter } from 'rxjs/operators';
-// import { paint} from '../assets/canvas';
 
 @Component({
   selector: 'app-root',
@@ -19,24 +18,16 @@ export class AppComponent implements OnInit, AfterViewInit {
   canvas: any;
 
   ngOnInit() {
-
     this.mouseMove();
   }
 
   ngAfterViewInit() {
     this.canvas = this.myCanvasEl.nativeElement;
     this.ctx = this.canvas.getContext('2d');
-    // console.log(this.ctx);
     this.canvas.width = window.innerWidth - 20;
     this.canvas.height = window.innerHeight - 20;
     this.ctx.lineJoin = 'round';
     this.ctx.lineCap = 'round';
-    this.ctx.lineWidth = 30;
-    // console.log(this.canvas.width);
-    // console.log(this.canvas.height);
-    // console.log(this.ctx.lineJoin);
-    // console.log(this.ctx.lineCap);
-    // console.log(this.ctx.lineWidth);
   }
 
   private mouseMove(): void {
@@ -50,8 +41,6 @@ export class AppComponent implements OnInit, AfterViewInit {
         }));
     mouseMove.subscribe((pos) => {
       this.left = pos.left, this.top = pos.top;
-      // this.stylObj = { {pos.top}px,  {pos.left}px` };
-       //console.log("this.left = " + this.left + " this.top = " + this.top)
     });
     const down$ = fromEvent(document, 'mousedown')
     const up$ = fromEvent(document, 'mouseup')
@@ -62,22 +51,19 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
   paint(): void {
     let infiniteX = Infinity;
-    let infiniteY = Infinity;    
+    let infiniteY = Infinity;
     let colorHue = 230;
     this.ctx.strokeStyle = `hsl(${colorHue}, 100%, 60%)`;
-    //console.log(this.ctx.strokeStyle);
-      this.ctx.beginPath();
-         
-      if (Math.abs(infiniteX - this.left) < 100 && Math.abs(infiniteY - this.top) < 100) {
-        this.ctx.moveTo(infiniteX, infiniteY);
-        }
-      this.ctx.lineTo(this.left,  this.top);
-      this.ctx.stroke();
-      infiniteX = this.left;
-      infiniteY =  this.top;
-      colorHue++;
-      console.log("infiniteX = " + infiniteX + " infiniteY = " + infiniteY);
-    
+    this.ctx.beginPath();
+    if (Math.abs(infiniteX - this.left) < 100 && Math.abs(infiniteY - this.top) < 100) {
+      this.ctx.moveTo(infiniteX, infiniteY);
+    }
+    this.ctx.lineTo(this.left, this.top);
+    this.ctx.stroke();
+    infiniteX = this.left;
+    infiniteY = this.top;
+    colorHue++;
+    console.log("infiniteX = " + infiniteX + " infiniteY = " + infiniteY);
   }
 
 }
